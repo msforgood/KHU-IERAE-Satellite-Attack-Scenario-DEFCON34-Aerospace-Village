dffc3288b20f9f66dc445effe8520ba162645ff3
SeoPPak <andrew201239@gmail.com>
2026-07-27 16:57:21 +0900

    FIX:커몬 삭제

--- common/attacker/gpredict/src/gtk-sat-module-popup.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GTK_SAT_MODULE_POPUP_H__
-#define __GTK_SAT_MODULE_POPUP_H__ 1
-
-void            gtk_sat_module_popup(GtkSatModule * module);
-gboolean        module_window_config_cb(GtkWidget *, GdkEventConfigure *,
-                                        gpointer);
-
-#endif
--- common/attacker/gpredict/src/gui.h
+++
@@ -1,8 +0,0 @@
-#ifndef GUI_H
-#define GUI_H 1
-
-#include <gtk/gtk.h>
-
-GtkWidget *gui_create(GtkWidget *window);
-
-#endif
